90cbbefc63dca85cdb212d8afbf21c25709d2f06
Abdulmalik <abdulmalikgairatov@gmail.com>
2025-09-09 15:04:57 +0500

    manyac

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,58 +1,96 @@
-const { sendErrorResponse } = require("../helpers/send.error.response");
-const Admin = require("../models/admin");
-const bcrypt = require("bcrypt");
-const jwtService = require("../services/jwt.service");
-
-const login = async (req, res) => {
-  //UnkanughtExtpation
-
-  try {
-    setTimeout(function () {
-      throw new Error("UnkanughtExtpation");
-    }, 1000);
-  } catch (error) {
-    console.log(error);
-  }
-
-  //unhandledRejection
-
-  new Promise((_, reject) => {
-    reject(new Error("unhandledRejection example"));
-  });
-
+const sign_up = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const admin = await Admin.findOne({ where: { email } });
-    if (!admin) {
+
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return sendErrorResponse({ message: "User already exists" }, res, 400);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 7);
+    const newUser = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    res.status(201).send({ message: "New user added", data: newUser });
+  } catch (err) {
+    sendErrorResponse(err, res, 400);
+  }
+};
+
+const sign_in = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
       return sendErrorResponse(
-        { message: "Email yoki password noto'g'ri" },
+        { message: "Invalid email or password" },
         res,
         401
       );
     }
-    const verifyPassword = await bcrypt.compare(password, admin.password);
 
+    const verifyPassword = await bcrypt.compare(password, user.password);
     if (!verifyPassword) {
       return sendErrorResponse(
-        { message: "Email yoki password noto'g'ri" },
+        { message: "Invalid email or password" },
         res,
         401
       );
     }
 
     const payload = {
-      id: admin.id,
-      email: admin.email,
-      is_active: admin.is_active,
-      is_creator: admin.is_creator,
+      id: user.id,
+      email: user.email,
+      is_active: user.is_active,
+      role: "user",
     };
+
     const tokens = jwtService.generateTokens(payload);
-    res.status(200).send(tokens);
-  } catch (error) {
-    sendErrorResponse(error, res, 500);
+
+    const hashedRefreshToken = await bcrypt.hash(tokens.refreshToken, 7);
+    user.refresh_token = hashedRefreshToken;
+    await user.save();
+
+    res.cookie("refreshToken", tokens.refreshToken, {
+      maxAge: config.get("cookie_refresh_token_time"),
+      httpOnly: true,
+    });
+
+    res.send({ message: "User logged in", accessToken: tokens.accessToken });
+  } catch (err) {
+    sendErrorResponse(err, res, 500);
   }
 };
 
-module.exports = {
-  login,
+const sign_out = async (req, res) => {
+  try {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      return sendErrorResponse(
+        { message: "Cookie refresh token not found" },
+        res,
+        400
+      );
+    }
+
+    const verifiedRefreshToken = await jwtService.verifyRefreshToken(
+      refreshToken
+    );
+    const user = await User.findByPk(verifiedRefreshToken.id);
+
+    if (!user) {
+      return sendErrorResponse({ message: "User not found" }, res, 404);
+    }
+
+    user.refresh_token = null;
+    await user.save();
+
+    res.clearCookie("refreshToken");
+    res.status(200).json({ message: "Successfully logged out" });
+  } catch (error) {
+    sendErrorResponse(error, res, 500);
+  }
 };
